fec8c1d29ce56360e278911e2e54ec4ac30d4357
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2023-08-04 08:52:35 +0300

    add 'DiagnosticUnnecessary' to vscode color theme

--- lua/plugins/colorschemes/vscode.lua
+++ lua/plugins/colorschemes/vscode.lua
@@ -6,11 +6,14 @@ require('vscode').setup({
     -- use colors from this colorscheme by requiring vscode.colors!
     ['@constant.builtin'] = { fg = vsc.vscBlue, bg = vsc.vscNone },
     ['@variable.builtin'] = { fg = vsc.vscBlue, bg = vsc.vscNone },
-    ['Type'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+    -- ['Type'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
     -- ['Typedef'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
     -- ['@type'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
     -- ['@type.builtin'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
     -- ['@type.qualifier'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+    ['DiagnosticUnnecessary'] = { fg = '#888888'--[[ vsc.vscGray ]], bg = vsc.vscNone, undercurl=true },
   }
 })
 require('vscode').load()
+
+-- vim.api.nvim_set_hl(0, 'DiagnosticUnneccessary', { fg = vsc.vscGray, bg = vsc.vscNone })
