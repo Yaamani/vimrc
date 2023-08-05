4c6923984d60f7ef64096d6a288ec2ca259fff02
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2023-08-05 10:43:07 +0300

    remove c-j nvim tree shortcut

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,7 +1,7 @@
 require("nvim-tree").setup({
   on_attach = function (bufnr)
     require('nvim-tree.api').config.mappings.default_on_attach(bufnr)
-    vim.keymap.set('n', '<C-j>', require('nvim-tree.api').node.open.edit, { desc = 'Open',  buffer = bufnr, noremap = true, silent = true, nowait = true })
+    -- vim.keymap.set('n', '<C-j>', require('nvim-tree.api').node.open.edit, { desc = 'Open',  buffer = bufnr, noremap = true, silent = true, nowait = true })
   end,
   view = {
     preserve_window_proportions = true,
