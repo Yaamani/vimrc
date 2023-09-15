f43d1f836dd8676eb0eec0f3d6f011bf868c8c59
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2023-09-15 15:01:52 +0300

    prevent tab from secleting the next item in nvim-cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -113,6 +113,10 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
+  -- window = {
+  --   completion = cmp.config.window.bordered(),
+  --   documentation = cmp.config.window.bordered(),
+  -- },
   mapping = cmp.mapping.preset.insert {
     ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
     ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
@@ -124,18 +128,18 @@ cmp.setup {
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
+      --[[ if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      else ]]if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
+      --[[ if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      else ]]if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
