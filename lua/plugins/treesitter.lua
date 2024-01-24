ba93a64cff8b79a8af911295de273f236046ebbe
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2024-01-24 10:14:31 +0200

    add java to treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'javascript', 'tsx', 'typescript', --[[ 'help', ]]
+  ensure_installed = { 'c', 'cpp', 'java', 'go', 'lua', 'python', 'rust', 'javascript', 'tsx', 'typescript', --[[ 'help', ]]
     'vim', 'json' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
